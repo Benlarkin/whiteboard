b9328e93b488306aa7f987f865aeb4ec80fb1f04
Benjamin Larkin <Ben>
2019-04-04 18:40:04 -0700

    car and cdr impementation using lambda functions

---
+++ carcdr.py
@@ -0,0 +1,17 @@
+'''
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+'''
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair):
+    return pair(lambda a, b: a)
+
+def cdr(pair):
+    return pair(lambda a, b: b)
+
+#print(cdr(cons("a", "b")))
